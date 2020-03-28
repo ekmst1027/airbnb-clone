acfb86adb7390d0f3bb27cc1e3285c550dd8688d
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-03-28 13:29:46 +0900

    #16.2 Verifying Email part One

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,4 @@ EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "noreply@sandbox698506e4a9fb42eebe12c3a7557d0da7.mailgun.org"
