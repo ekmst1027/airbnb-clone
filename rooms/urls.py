de6b4d73c9d747d4766fcd87045a597e8486b276
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-04-05 00:12:02 +0900

    #23.4 Edit Photo

--- rooms/urls.py
+++ rooms/urls.py
@@ -10,7 +10,12 @@ urlpatterns = [
     path(
         "<int:room_pk>/photos/<int:photo_pk>/delete/",
         views.delete_photo,
-        name="delete_photo",
+        name="delete-photo",
+    ),
+    path(
+        "<int:room_pk>/photos/<int:photo_pk>/edit/",
+        views.EditPhotoView.as_view(),
+        name="edit-photo",
     ),
     path("search/", views.SearchView.as_view(), name="search"),
 ]
