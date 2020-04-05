bcb4a79e10653dbfa7dd3f17f97d3a90e41db36f
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-04-05 14:44:51 +0900

    #24.1 Room Calendar part Two

--- rooms/models.py
+++ rooms/models.py
@@ -114,6 +114,6 @@ class Room(core_models.TimeStampedModel):
         return photos
 
     def get_calendars(self):
-        calendar = Calendar(2020, 4)
-        print(calendar.get_month())
-        return False
+        this_month = Calendar(2020, 4)
+        next_month = Calendar(2020, 5)
+        return [this_month, next_month]
